b4e01ed0d63f59b2d35e48ea338447f938f45e80
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-25 19:05:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  attr_accessor :name, :hair_color, :eye_color, :height, :complexion
+
+  def initialize(attributes)
+    attributes.each {|key, values| self.send(("#{key}="), values)}
+  end
+  
 end
